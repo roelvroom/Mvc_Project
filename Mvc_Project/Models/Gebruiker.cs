beb00bad026fdbace4d2bb2cbfe0283f84708ac7
Roel Vroom <r0950679@student.thomasmore.be>
2024-08-21 12:45:21 +0200

    Test eerste class gebruiker

---
+++ Mvc_Project/Models/Gebruiker.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Mvc_Project.Models
+{
+    public class Gebruiker
+    {
+        public int GebruikerId { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string Initialen { get; set; }
+        public string GebruikersNaam { get; set; }
+        public string Wachtwoord { get; set; }
+        public string Email { get; set; }
+        public bool Verwijder { get; set; }
+    }
+}
