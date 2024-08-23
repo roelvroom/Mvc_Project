e9cf4fcf90434af10e967683271e03cd214b6400
Roel Vroom <r0950679@student.thomasmore.be>
2024-08-23 12:17:52 +0200

    database connectie gemaakt

---
+++ Mvc_Project/Data/Mvc_ProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mvc_Project.Models;
+
+namespace Mvc_Project.Data
+{
+    public class Mvc_ProjectContext : DbContext
+    {
+        public Mvc_ProjectContext (DbContextOptions<Mvc_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aankoop> Aankoop { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Aankoop>().ToTable("Aankoop");
+        }
+    }
+}
---
+++ Mvc_Project/Migrations/20240822105029_initialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mvc_Project.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aankoop",
+                columns: table => new
+                {
+                    AankoopId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VakId = table.Column<int>(type: "int", nullable: false),
+                    NaamAanvragerId = table.Column<int>(type: "int", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Reden = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GoedGekeurd = table.Column<bool>(type: "bit", nullable: false),
+                    Verwijderd = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aankoop", x => x.AankoopId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aankoop");
+        }
+    }
+}
