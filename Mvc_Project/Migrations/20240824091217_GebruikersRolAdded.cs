fd9e3fadb15c47b66b394f04d962e200af39729c
Roel Vroom <r0950679@student.thomasmore.be>
2024-08-24 11:29:33 +0200

    Gebruikers aangemaakt

---
+++ Mvc_Project/Migrations/20240824091217_GebruikersRolAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mvc_Project.Migrations
+{
+    public partial class GebruikersRolAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rol",
+                table: "Gebruiker",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rol",
+                table: "Gebruiker");
+        }
+    }
+}
